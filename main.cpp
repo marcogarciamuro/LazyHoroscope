4cf9e0307a9711de7e6cf79ecd363c80b03630c9
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-09-19 16:13:00 -0700

    Asks for first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Author:Marco Garcia
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
+  string FirstName;
+  string LastName;
 
   //get user input
+  cout<<"Please enter your first name"<<endl;
+  cin>>FirstName;
+  cout<<"Please enter your last name"<<endl;
+  cin>>LastName;
 
   //tell fortune
 
