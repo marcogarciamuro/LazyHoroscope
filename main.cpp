393cf6919e4973942aa8ab37e3c4bedc2a62b032
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-09-24 17:10:33 -0700

    Determines Low profile or true love or famous, true love found or not

--- main.cpp
+++ main.cpp
@@ -9,17 +9,32 @@ int main()
   //declare variables
   string FirstName;
   string LastName;
+  int index;
 
   //get user input
   cout<<"Please enter your first name"<<endl;
   cin>>FirstName;
   cout<<"Please enter your last name"<<endl;
   cin>>LastName;
+  index = LastName.length()-1;
 
   cout<<"Welcome, "<<FirstName[0]<<"."<<LastName[0]<<" here is your fortune..."<<endl;
 
   //tell fortune
   cout<<"Your lucky number is "<<FirstName.length()<<endl;
-
-  return 0;
+  if(FirstName[0] == 'A'||FirstName[0] == 'a'||FirstName[0] == 'E'||FirstName[0] == 'e'||FirstName[0] == 'I'||
+     FirstName[0] == 'i'||FirstName[0] == 'O'||FirstName[0] == 'o'||FirstName[0] == 'U'||FirstName[0] == 'u')      
+ {      
+  cout<<"You are destined to be famous!"<<endl;
+ }
+  else
+ {
+  cout<<"You should keep a low profile."<<endl;
+ }
+  if(LastName[index] == 'A'|| LastName[index] == 'a'|| LastName[index] == 'E'|| LastName[index] == 'e'|| LastName[index] == 'I'||
+     LastName[index] == 'i'|| LastName[index] == 'O'|| LastName[index] == 'o'|| LastName[index] == 'U'|| LastName[index] == 'u')
+ {
+   cout<<"You have already met your true love."<<endl;
+ }
+  cout<<"Have a good day!"<<endl;
 }
